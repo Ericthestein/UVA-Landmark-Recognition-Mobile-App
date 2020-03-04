b9dfee4f6b18053106992c36278fae4e8d9a3813
Eric Stein <ericthestein@gmail.com>
2020-03-03 21:41:43 -0500

    added leaderboard button and overlay and functionality

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: 'AIzaSyAxbTJrB3i9cvOYdRjdowMAlLXvkJdZzVM', // The API Key used by your Firebase project
-    authDomain: 'uva-landmark-images.firebaseapp.com', // The authDomain of your Firebase project
-    databaseURL: 'https://uva-landmark-images.firebaseio.com/', // The url to your Firebase project's database
-    storageBucket: 'gs://uva-landmark-images.appspot.com/', // The storage bucket of your Firebase project (typically in the form of "gs://" + authDomain + "/")
+    apiKey: '', // The API Key used by your Firebase project
+    authDomain: '', // The authDomain of your Firebase project
+    databaseURL: '', // The url to your Firebase project's database
+    storageBucket: '', // The storage bucket of your Firebase project (typically in the form of "gs://" + authDomain + "/")
 }
 
 export default firebaseConfig
